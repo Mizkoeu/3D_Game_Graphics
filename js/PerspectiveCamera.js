87f786c6ec14ace2453af391d7b276a2b8864d55
Mike Zou <zoutianh@grinnell.edu>
2017-11-27 22:50:56 +0100

    fixed merge issue and shadow bug

--- js/PerspectiveCamera.js
+++ js/PerspectiveCamera.js
@@ -1,11 +1,7 @@
 "use strict"
 var PerspectiveCamera = function()
 {
-<<<<<<< HEAD
   this.position = new Vec3(0.0, 0.8, -10.0);
-=======
-  this.position = new Vec3(0.0, 0.8, -5.0);
->>>>>>> 1dd06c60f957dcee2b64f6b166649e36c3cf2f06
   this.ahead = new Vec3(0.0, 0.0, 1.0);
   this.right = new Vec3(-1.0, 0.0, 0.0);
   this.up = new Vec3(0.0, 1.0, 0.0);
